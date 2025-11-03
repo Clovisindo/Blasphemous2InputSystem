72fe0be53d3033bbb7bb9e001c52370d8948defa
Clovis <kiliranshe@gmail.com>
2025-11-03 10:07:02 +0100

    Añadimos onTriggerStay para controlar las colisiones internas despues de la invulnerabilidad del dash o de recibir un golpe.

--- Assets/Scripts/Application/Enemy.cs
+++ Assets/Scripts/Application/Enemy.cs
@@ -1,6 +1,8 @@
 ﻿using Game.Core;
 using Game.Core.Orchestrator;
 using Game.Events;
+using System;
+using System.Collections.Generic;
 using UnityEngine;
 using static Game.Events.PlayerEvents.PlayerEvents;
 
@@ -8,9 +10,13 @@ namespace Game.Application.Enemies
 {
     public class Enemy : MonoBehaviour, ICoreDependent
     {
+        private Dictionary<Guid, float> _nextAllowedHit = new();
+        IEventBus _eventBus;
         public int damage = 10;
         public float stun = 0.5f;
-        IEventBus _eventBus;
+        public float damageInterval = 0.5f;
+        public float time_last_damage = 0f;
+        
 
         private void Awake()
         {
@@ -27,6 +33,42 @@ namespace Game.Application.Enemies
             var view = other.GetComponent<PlayerView>();
             if (view == null) { return; }
 
+            InvokeDamageIntent(view);
+        }
+
+        private void OnTriggerStay2D(Collider2D other)
+        {
+            time_last_damage += Time.deltaTime;
+            var view = other.GetComponent<PlayerView>();
+            if (view == null) { return; }
+
+            //control para si tuvieramos otros jugadores o daño de fuego amigo entre enemigos
+            var id = view.GetPlayerId();
+            if (!_nextAllowedHit.TryGetValue(id, out var nextTime))
+                nextTime = 0;
+
+            if (time_last_damage >= nextTime)
+            {
+                InvokeDamageIntent(view);
+
+                _nextAllowedHit[id] = damageInterval;
+                time_last_damage = 0f;
+            }
+        }
+
+        void OnTriggerExit2D(Collider2D other)
+        {
+            var view = other.GetComponent<PlayerView>();
+            if (view != null)
+                _nextAllowedHit.Remove(view.GetPlayerId());
+        }
+
+        /// <summary>
+        /// calculos necesarios e invocacion de evento damageIntent
+        /// </summary>
+        /// <param name="view"></param>
+        private void InvokeDamageIntent(PlayerView view)
+        {
             var direction = (view.transform.position - transform.position).normalized;
             var intent = new DamageIntentEvent(view.GetPlayerId(), damage, stun, direction, gameObject);//editando este evento y extendiendo los enemigos, podriamos hacer distintos tipos de daño, resistencias en dominio segun el daño,etc
             _eventBus.Publish(intent);
